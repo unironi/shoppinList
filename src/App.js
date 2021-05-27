122778dfef1093b65069764a8f30f1e0bdcce0b6
ororo the unicorn <45035470+unironi@users.noreply.github.com>
2021-05-28 01:30:00 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,53 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './components/navbar'
+import Counters from './components/counters';
+import React, { Component } from 'react';
+
+class App extends Component {
+  state = { 
+    counters: [{id: 1, value: 0}, {id: 2, value: 0}, {id: 3, value: 0}, {id: 4, value: 0}]
+ }
+
+ handleDelete = (counterId) => {
+     const counters = this.state.counters.filter(counter => counter.id !== counterId);
+     this.setState({ counters });
+ }
+
+ handleReset = () => {
+     const counters = this.state.counters.map(counter => {
+         counter.value = 0;
+         return counter;
+    })
+    this.setState({ counters });
+ }
+
+ handleIncrement = (counter) => {
+     const counters = [...this.state.counters];
+     const index = counters.indexOf(counter);
+     //counters[index] = {...counter};
+     counters[index].value++;
+     this.setState({ counters });
+ }
+
+  render() {
+    return (
+      <React.Fragment>
+        <Navbar 
+          totalcounters={this.state.counters.filter(c => c.value > 0).length}
+        />
+        <main className="container">
+          <Counters 
+            counters={this.state.counters}
+            onReset={this.handleReset}
+            onDelete={this.handleDelete}
+            onIncrement={this.handleIncrement}
+          />
+        </main>
+      </React.Fragment>
+      
+    );
+  }
+}
+
+export default App;
---
+++ src/components/navbar.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Navbar extends Component {
+    render() { 
+        return ( 
+            <nav className="navbar navbar-light bg-light">
+                <a className="navbar-brand" href="#">
+                    Navbar{" "} 
+                    <span className="btn btn-secondary btn-sm m-2">{this.props.totalCounters}</span>
+                </a>
+            </nav> 
+      );
+    }
+}
+ 
+export default Navbar;
